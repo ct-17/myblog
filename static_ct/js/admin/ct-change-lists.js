76cbc013f6041ccf6cebe3189f1d6dd6839d826b
ct27 <manhcuong88c@gmail.com>
2019-10-28 22:34:35 +0700

    [FIX] sua loi nut check all khong hoat dong o list trong admin.

--- static_ct/js/admin/ct-change-lists.js
+++ static_ct/js/admin/ct-change-lists.js
@@ -1,8 +1,27 @@
-$(options.allToggle).show().on('click', function () {
-    checker($(this).prop("checked"));
-    updateCounter();
-});
+$(document).ready(function(){
+    // Check or Uncheck All checkboxes
+    $("#action-toggle").change(function(){
+        var checked = $(this).is(':checked');
+        if (checked){
+            $(".action-select").each(function(){
+                $(this).prop("checked", true);
+            });
+        }
+        else {
+            $(".action-select").each(function(){
+                $(this).prop("checked",false);
+            });
+        }
+    });
 
-$.fn.actions.defaults = {
-    allToggle: "#action-toggle",
-}
+    // Changing state of CheckAll checkbox
+    $(".action-select").click(function(){
+        if ($(".action-select").length == $(".action-select:checked").length){
+            $("#action-toggle").prop("checked", true);
+        }
+        else {
+            $("#action-toggle").removeAttr("checked");
+        }
+    });
+})
+
--- storage/static_root/js/admin/ct-change-lists.js
+++ storage/static_root/js/admin/ct-change-lists.js
@@ -1,4 +1,27 @@
-$('#action-toggle').show().on('click', function () {
-    checker($(this).prop("checked"));
-    updateCounter();
-});
+$(document).ready(function(){
+    // Check or Uncheck All checkboxes
+    $("#action-toggle").change(function(){
+        var checked = $(this).is(':checked');
+        if (checked){
+            $(".action-select").each(function(){
+                $(this).prop("checked", true);
+            });
+        }
+        else {
+            $(".action-select").each(function(){
+                $(this).prop("checked",false);
+            });
+        }
+    });
+
+    // Changing state of CheckAll checkbox
+    $(".action-select").click(function(){
+        if ($(".action-select").length == $(".action-select:checked").length){
+            $("#action-toggle").prop("checked", true);
+        }
+        else {
+            $("#action-toggle").removeAttr("checked");
+        }
+    });
+})
+
